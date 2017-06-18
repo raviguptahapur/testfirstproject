42a0e3f2cfdd99d9da251bb69bfe07a018ad55fa
Ravi Gupta <ravigupta@Ravis-MacBook-Pro.local>
2017-06-18 10:31:27 +0530

    my first commit

---
+++ DemoFirstGitProject/DemoFirstGitProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoFirstGitProject
+//
+//  Created by Ravi Gupta on 18/06/17.
+//  Copyright © 2017 Ravi Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
